c7e835036bdc86fb388785e8cb8f1b8111ac278b
RobinsonGr <robinsonandresgr@gmail.com>
2024-02-24 00:10:52 -0500

    s

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 const URL = 'http://localhost:3000'
 
-function getProducts() {
+function getProducts(categoryID) {
     //Retrieveing the product list throughtout the classical method  of chaning 'then'
-   return fetch(`${URL}/products`)
+   return fetch(`${URL}/products/${categoryID}`)
     //Parsing the response as JSON
         .then((response) => response.json())
         //resolving the promise with the retrieved data
