50026f1a4027103d2bc9b37b2d33494e087229ae
Robinson Gr <77526151+RobinsonGr@users.noreply.github.com>
2024-04-29 23:59:25 -0500

    Update test.js

--- src/test/test.js
+++ src/test/test.js
@@ -25,3 +25,59 @@ export default {
   ],
   reporters: ['default', 'github-actions'],
 };
+
+import baseConfig from './jest.config.mjs';
+
+/** @type {import('jest').Config} */
+export default {
+  projects: [
+    {
+      displayName: {
+        color: 'blue',
+        name: 'ts-integration',
+      },
+      modulePathIgnorePatterns: baseConfig.modulePathIgnorePatterns,
+      roots: ['<rootDir>/e2e/__tests__'],
+      testMatch: ['<rootDir>/e2e/__tests__/ts*'],
+    },
+    {
+      displayName: {
+        color: 'blue',
+        name: 'type-tests',
+      },
+      modulePathIgnorePatterns: baseConfig.modulePathIgnorePatterns,
+      roots: ['<rootDir>/packages'],
+      runner: 'jest-runner-tsd',
+      testMatch: ['**/packages/jest-types/__typetests__/each.test.ts'],
+    },
+  ],
+  reporters: ['default', 'github-actions'],
+};
+
+import baseConfig from './jest.config.mjs';
+
+/** @type {import('jest').Config} */
+export default {
+  projects: [
+    {
+      displayName: {
+        color: 'blue',
+        name: 'ts-integration',
+      },
+      modulePathIgnorePatterns: baseConfig.modulePathIgnorePatterns,
+      roots: ['<rootDir>/e2e/__tests__'],
+      testMatch: ['<rootDir>/e2e/__tests__/ts*'],
+    },
+    {
+      displayName: {
+        color: 'blue',
+        name: 'type-tests',
+      },
+      modulePathIgnorePatterns: baseConfig.modulePathIgnorePatterns,
+      roots: ['<rootDir>/packages'],
+      runner: 'jest-runner-tsd',
+      testMatch: ['**/packages/jest-types/__typetests__/each.test.ts'],
+    },
+  ],
+  reporters: ['default', 'github-actions'],
+};
