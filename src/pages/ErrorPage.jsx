b70ef1452c91c69dfa2da3753de30d2ce349ac95
RobinsonGr <robinsonandresgr@gmail.com>
2024-04-29 23:54:16 -0500

    errorPage updated

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -9,3 +9,15 @@ function ErrorPage () {
 };
 
 export default ErrorPage;
+
+import React from 'react'
+
+function ErrorPage () {
+    return (
+        <div>
+            <h1>"Ops, something went wrong!"</h1>
+        </div>        
+    );
+};
+
+export default ErrorPage;
