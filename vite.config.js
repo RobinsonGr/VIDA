fe9ee7cd3ee8f40a236077e873c15983f4f538cc
RobinsonGr <robinsonandresgr@gmail.com>
2024-07-23 16:19:25 -0500

    using _redirects file to be a le to use react router into the deployment using csr, because i got the problem it didnt work well in

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
   },
-  publicDir: '',
 })
