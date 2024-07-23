273bf318aca15f89902d99cd99ddb198db5b9675
RobinsonGr <robinsonandresgr@gmail.com>
2024-07-23 16:05:29 -0500

    using _redirects file to be a le to use react router into the deployment using csr, because i got the problem it didnt work well in render due to spas

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-
-// https://vitejs.dev/config/
-
-dotenv.config();
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  publicDir: '',
 })
